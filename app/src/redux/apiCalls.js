031b97d8bd1456ba2e33c61eb3bbc4eb571a89b1
walidismail11 <walid.ismail@net.usj.edu.lb>
2022-01-06 21:26:03 +0200

    first committ

--- app/src/redux/apiCalls.js
+++ app/src/redux/apiCalls.js
@@ -6,9 +6,10 @@ import { publicRequest } from "../requestMethod";
 export const login = async (dispatch, user) => {
   dispatch(loginStart());
   try {
-    const res = await publicRequest.post("/auth/login", user);
+    const res = await publicRequest.post("/auth/login", user, { headers: {'Content-Type': 'application/json'}});
     dispatch(loginSuccess(res.data));
   } catch (err) {
+    console.log(err)
     dispatch(loginFailure());
   }
 };
