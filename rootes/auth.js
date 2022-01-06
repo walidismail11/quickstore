49e43d1c170e48cb0c4d1a94f3ef9b9b3c50e2be
walidismail11 <walid.ismail@net.usj.edu.lb>
2022-01-06 22:01:03 +0200

    first committ

--- rootes/auth.js
+++ rootes/auth.js
@@ -24,7 +24,7 @@ router.post("/register", async (req, res) => {
 
 //LOGIN
 
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
     try{
         const user = await User.findOne(
             {
