ff2d7a7eec08d037093daa5a06949257db3f11ef
walidismail11 <walid.ismail@net.usj.edu.lb>
2022-01-07 15:53:23 +0200

    first committ

--- app/src/pages/Product.jsx
+++ app/src/pages/Product.jsx
@@ -135,12 +135,12 @@ const Product = () => {
   const dispatch = useDispatch();
   
   useEffect(() => {
-    console.log(colors);
     const getProduct = async () => {
       try {
         const res = await publicRequest.get("/products/find/" + id);
         setProduct(res.data);
         const colors = product.color
+        console.log(colors);
       } catch {}
     };
     getProduct();
